f3a419bbb511160c0ae199b27201c27ff3ad9a27
Kenny Capote <kenny.capote@gmail.com>
2019-02-03 18:35:16 -0300

    chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,27 +1,26 @@
 var socket = io();
 
-
 var params = new URLSearchParams(window.location.search);
 
-
-if (!params.has('nombre') || !params.has('sala') ) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre  y sala son necesarios');
-
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
-
 
 });
 
@@ -34,8 +33,8 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -43,23 +42,22 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 
 });
 
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log('Servidor:', personas);
-
+    renderizarUsuarios(personas);
+    //console.log(personas);
 });
 
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-//Mensajes privados
+    console.log('Mensaje Privado:', mensaje);
 
-socket.on('mensajePrivado', function(mensaje){
-
-    console.log('Mensaje Privado: ', mensaje);
-    
-})
+});
